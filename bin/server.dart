26ebdce5181c4e3821897de5e4e3d6f99026df98
lucas cardoso <lucas.cardo123@hotmail.com>
2022-05-10 14:12:53 -0400

    add credencial a main

--- bin/server.dart
+++ bin/server.dart
@@ -10,15 +10,14 @@ import 'app/lakes/remove.lake.dart';
 import 'app/peixes/create.fish.dart';
 import 'app/peixes/list.fishes.dart';
 import 'app/peixes/remove.fish.dart';
+import 'core/configs/credenciais.dart';
 import 'core/middlewares/authorize.dart';
 import 'core/services/db_service.dart';
 
 final db = DbService();
 void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
   await db.inicialise();
-// Configure routes.
+  // Configure routes.
   final _router = Router()
     ..get('/lakes', ListLakes(db))
     ..get('/fishes', ListFishes(db))
@@ -26,12 +25,9 @@ void main(List<String> args) async {
     ..post('/fish.create', CreateFish(db))
     ..delete('/fish.remove', RemoveFishes(db))
     ..delete('/lake.remove', RemoveLakes(db));
-
   // Configure a pipeline that logs requests.
   final _handler = Pipeline().addMiddleware(logRequests()).addMiddleware(Authorize()).addHandler(_router);
-
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(_handler, ip, port);
+  final server = await serve(_handler, InternetAddress.anyIPv4, Credenciais.portAplication);
   print('Server listening on port ${server.port}');
 }
